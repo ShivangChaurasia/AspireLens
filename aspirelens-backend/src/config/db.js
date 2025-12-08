435dda7827854e4800d2896ec36103e0ad2ce7ec
ShivangChaurasia <shiva17ng@gmail.com>
2025-12-09 01:13:08 +0530

    .env ignored

--- aspirelens-backend/src/config/db.js
+++ aspirelens-backend/src/config/db.js
@@ -2,7 +2,14 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const username = process.env.DB_USERNAME;
+    const password = process.env.DB_PASSWORD;
+    const dbName = process.env.DB_NAME;
+    const cluster = process.env.MONGO_CLUSTER;
+
+    const MONGO_URI = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(MONGO_URI);
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
     console.error("❌ Database Connection Error:", error.message);
