11f562c1bc42e0fce6c8a5d5c37174b20b1c3aa5
ShivangChaurasia <shiva17ng@gmail.com>
2026-02-23 01:00:35 +0530

    feat: Add Firebase initialization and Google authentication with a `signInWithGoogle` function.

--- aspirelens-frontend/src/firebase.js
+++ aspirelens-frontend/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBeHCdEQIVKN6E4-4lFUQUAm_oruO4TZc8",
-    authDomain: "aspirelens-80e6b.firebaseapp.com",
-    projectId: "aspirelens-80e6b",
-    storageBucket: "aspirelens-80e6b.firebasestorage.app",
-    messagingSenderId: "483654846089",
-    appId: "1:483654846089:web:ad10e2ea70ed56aceb2858",
-    measurementId: "G-EE7HVZMR05",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
